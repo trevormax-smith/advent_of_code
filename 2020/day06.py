e3950ff2c2c9e4d85f2b53e79c5eb4f14db0c5e5
Michael Bell <bellmr@gmail.com>
2020-12-06 09:07:01 -0500

    Fixed up day 6 a bit.

--- 2020/day06.py
+++ 2020/day06.py
@@ -1,48 +1,41 @@
 # Advent of Code 2020, Day 5
 # Michael Bell
 # 12/6/2020
+from collections import defaultdict, namedtuple
+from typing import List
+
 import helper
-from collections import defaultdict
 
 
-def parse_group_declarations(group_dec):
+group_declaration = namedtuple('GroupDeclaration', 'size, declarations')
 
-    dec = defaultdict(list)
 
-    ind_decs = group_dec.split('\n')
+def parse_group_declaration(group_decs: str) -> group_declaration:
+
+    q_to_people = defaultdict(list)  # Indexed on the question ID, a list of people IDs in the group that answered yes
+    ind_decs = [ind_dec for ind_dec in group_decs.split('\n') if ind_dec.strip()]
+    group_size = len(ind_decs)
 
     for person_id, ind_dec in enumerate(ind_decs):
         for q in ind_dec:
-            dec[q].append(person_id)
+            q_to_people[q].append(person_id)
     
-    return dec
+    return group_declaration(group_size, q_to_people)
 
 
-def parse_declaration_batch(all_decs):
+def parse_declaration_batch(all_decs: str) -> List[group_declaration]:
     decs = all_decs.split('\n\n')
-    group_decs = [parse_group_declarations(group_dec) for group_dec in decs]
+    group_decs = [parse_group_declaration(group_dec) for group_dec in decs]
     return group_decs
 
 
-def count_qs(group_decs):
-    return sum(len(decs) for decs in group_decs)
+def count_qs(group_decs: List[group_declaration]) -> int:
+    return sum(len(group.declarations) for group in group_decs)
 
 
-def count_unanimous_qs(group_decs):
-    unanimous_q_count = 0
+def count_unanimous_qs(group_decs: List[group_declaration]) -> int:
+    return sum(1 for group in group_decs for q in group.declarations if len(group.declarations[q]) == group.size)
 
-    for group_dec in group_decs:
-
-        person_ids = []
-        for q in group_dec:
-            person_ids.extend(group_dec[q])
-        
-        n_people = len(set(person_ids))
-
-        unanimous_q_count += sum(1 for q in group_dec if len(group_dec[q]) == n_people)
-
-    return unanimous_q_count
-        
 
 sample_declaration_batch = '''abc
 
@@ -61,6 +54,7 @@ a
 b'''
 sample_declarations = parse_declaration_batch(sample_declaration_batch)
 assert count_qs(sample_declarations) == 11
+assert count_unanimous_qs(sample_declarations) == 6
 
 declarations_batch = helper.read_input(6)
 declarations = parse_declaration_batch(declarations_batch)
