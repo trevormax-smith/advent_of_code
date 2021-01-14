9e68daf436b92660eeff63d776388c92a725c690
Michael Bell <bellmr@gmail.com>
2021-01-14 08:39:55 -0500

    2015 day 15, part 1 solution added.

---
+++ 2015/day15.py
@@ -0,0 +1,79 @@
+# Advent of Code 2015, Day 15
+# Michael Bell
+# 1/14/2021
+
+import helper
+
+
+class Ingredient(object):
+    def __init__(self, properties):
+        name, properties = properties.split(': ')
+        prop_scores = properties.split(', ')
+        self.properties = {}
+        for ps in prop_scores:
+            p, s = ps.split(' ')
+            self.properties[p] = int(s)
+        self.name = name
+    def __repr__(self):
+        return f'Ingredient<{self.name}>'
+
+
+def cookie_score(recipe):
+    '''
+    Here recipe is a dict keyed on Ingredients with value of # of tsps in the recipe.
+    '''
+    prod = 1
+    for prop in ['capacity', 'durability', 'flavor', 'texture']:
+        total = 0
+        for ingredient in recipe:
+            total += recipe[ingredient] * ingredient.properties[prop]
+        total = max([0, total])
+        prod *= total
+    return prod
+
+
+def optimize_recipe(ingredient_properties):
+
+    ingredients = [Ingredient(properties) for properties in ingredient_properties]
+
+    total_tsp = 100
+
+    n_ingredients = len(ingredients)
+
+    def recursive_for(remaining_tsp, ingredients, best, recipe=None): 
+
+        if len(ingredients) >= 1:
+            if recipe is None:
+                this_recipe = {}
+            else:
+                this_recipe = recipe.copy()
+
+            if len(ingredients) == 1:
+                this_recipe[ingredients[0]] = remaining_tsp
+                recursive_for(0, ingredients[1:], best, this_recipe)
+            else:
+                for i in range(remaining_tsp):
+                    this_recipe[ingredients[0]] = i
+                    recursive_for(remaining_tsp - i, ingredients[1:], best, this_recipe)
+        else:
+            recipe_score = cookie_score(recipe)
+            if recipe_score > best['score']:
+                best['score'] = recipe_score
+                best['recipe'] = recipe.copy()
+    
+    best = {'recipe': None, 'score': -1}
+    recursive_for(total_tsp, ingredients, best)
+
+    return best['score'], best['recipe']
+
+example_ingredients = '''Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'''.split('\n')
+
+ingredients = [Ingredient(properties) for properties in example_ingredients]
+recipe = {ingredients[0]: 44, ingredients[1]: 56}
+assert cookie_score(recipe) == 62842880
+assert optimize_recipe(example_ingredients)[0] == 62842880
+
+ingredient_list = helper.read_input_lines(15)
+best_score, best_recipe = optimize_recipe(ingredient_list)
+print('Part 1:', best_score, best_recipe)
